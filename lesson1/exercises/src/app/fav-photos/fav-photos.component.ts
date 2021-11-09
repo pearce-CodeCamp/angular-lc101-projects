f51b91f22d83e04d59bb081e67e2c65fe8ff8983
Pearce Odegard <pearceodegard@gmail.com>
2021-11-09 16:39:31 -0600

    Mostly completed bonuses, sort of... lots of css refactor

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -7,9 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Neat Fotos';
-  image1 = 'https://i.inews.co.uk/content/uploads/2017/11/The-Room.jpg';
+  image1 = 'https://media3.giphy.com/media/G2vaqcEICxOyA/giphy.gif';
   image2 = 'https://bs-uploads.toptal.io/blackfish-uploads/components/seo/content/og_image_file/og_image/777588/top-18-most-common-angularjs-developer-mistakes-41f9ad303a51db70e4a5204e101e7414.png';
-  image3 = 'https://blog.wildix.com/wp-content/uploads/2020/06/react-logo.jpg';
 
   constructor() { }
 
